f2512c58b1b1da8f66e622e18dcb34630aee7239
srujana <srujanar308@gmail.com>
2022-05-03 22:55:15 +0530

    first commit

---
+++ Day_3_OOP/src/oop_capgemini/Example1.java
@@ -0,0 +1,14 @@
+package oop_capgemini;
+
+public class Example1 {
+
+	Example1()
+	{
+		System.out.println("Welcome");
+	}
+	public static void main(String[] args) {
+		Example1 e=new Example1();
+
+	}
+
+}
