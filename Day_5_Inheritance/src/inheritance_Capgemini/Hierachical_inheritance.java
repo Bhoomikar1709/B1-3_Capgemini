61f71b690e500988884a7a3713c523a27e57fa98
srujana <srujanar308@gmail.com>
2022-05-04 21:07:14 +0530

    first commit

---
+++ Day_5_Inheritance/src/inheritance_Capgemini/Hierachical_inheritance.java
@@ -0,0 +1,39 @@
+package inheritance_Capgemini;
+
+class vehicle
+{
+	void main()
+	{
+		System.out.println("vehicle types");
+	}
+}
+class car extends vehicle
+{
+	void display()
+	{
+		System.out.println("BMW");
+	}
+}
+
+class bike extends vehicle
+{
+	void print()
+	{
+		System.out.println("Bullet");
+	}
+}
+
+
+public class Hierachical_inheritance {
+
+	public static void main(String[] args) {
+		car obj=new car();
+		bike obj1=new bike();
+		obj.main();
+		obj.display();
+		obj1.print();
+		
+
+	}
+
+}
