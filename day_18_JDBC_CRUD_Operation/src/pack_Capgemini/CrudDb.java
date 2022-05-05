ccf695d2d773c1eb1b01180e28644d8ff19377da
srujana <srujanar308@gmail.com>
2022-05-05 16:26:52 +0530

    first commit

---
+++ day_18_JDBC_CRUD_Operation/src/pack_Capgemini/CrudDb.java
@@ -0,0 +1,54 @@
+package pack_Capgemini;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class CrudDb {
+
+	public static void main(String[] args) {
+		String dbURL="jdbc:mysql://localhost:3306/bhoomi";
+		String username="root";
+        String password="bhoomikar@1709";
+        try
+        {
+        Connection con=DriverManager.getConnection(dbURL, username, password);
+        String sql="SELECT * FROM Emp1";
+        Statement st=con.createStatement();
+        ResultSet r=st.executeQuery(sql);
+        int count=1;
+        while(r.next())
+        {
+        	
+        	String name=r.getString(2);
+        	String password1=r.getString(3);
+        	String email=r.getString(4);
+        	String result="User: %d %s %s %s";
+        	System.out.println(String.format(result,count++,name,password1,email));
+        }
+        con.close();
+       /* String sql="INSERT INTO Emp1(user_name,password,email)values(?,?,?)";
+         PreparedStatement s=con.prepareStatement(sql);
+       
+        s.setString(1,"bumi");
+        s.setString(2,"bumi@123");
+        s.setString(3,"bumi@gmail.com");
+        int rows=s.executeUpdate();
+        if(rows>0)
+        {
+        	System.out.println("A new user information entered successfully");
+        }
+        con.close();*/
+        }
+           catch(SQLException e)
+           {
+        	   System.out.println("Exception occured" +e);
+           }
+	}
+}
+
+
