8911ce370a11456ba00856b6d93520c3190f1403
srujana <srujanar308@gmail.com>
2022-05-10 23:32:04 +0530

    first commit

--- day_18_JDBC_CRUD_Operation/src/pack_Capgemini/CrudDb.java
+++ day_18_JDBC_CRUD_Operation/src/pack_Capgemini/CrudDb.java
@@ -14,10 +14,27 @@ public class CrudDb {
 		String dbURL="jdbc:mysql://localhost:3306/bhoomi";
 		String username="root";
         String password="bhoomikar@1709";
+        
+        
         try
         {
         Connection con=DriverManager.getConnection(dbURL, username, password);
-        String sql="SELECT * FROM Emp1";
+        String sql="DELETE from Emp1 WHERE user_id=2";
+        PreparedStatement s=con.prepareStatement(sql);
+        int rows=s.executeUpdate();
+        if(rows>0)
+        {
+        	System.out.println("A new user information deleted successfully");
+        }
+        con.close();
+        }
+        
+        
+        
+       /* try
+        {
+        Connection con=DriverManager.getConnection(dbURL, username, password);
+        String sql="SELECT *from Emp1";
         Statement st=con.createStatement();
         ResultSet r=st.executeQuery(sql);
         int count=1;
@@ -31,6 +48,44 @@ public class CrudDb {
         	System.out.println(String.format(result,count++,name,password1,email));
         }
         con.close();
+        }*/
+        
+        
+        
+       /* try
+        {
+        Connection con=DriverManager.getConnection(dbURL, username, password);
+        String sql="UPDATE Emp1 SET user_name=?,password=? WHERE user_id=1";
+        PreparedStatement s=con.prepareStatement(sql);
+        
+        s.setString(1,"bumishetty");
+        s.setString(2,"bumishetty@123");
+       // s.setInt(3,1);
+        
+        int rows=s.executeUpdate();
+        if(rows>0)
+        {
+        	System.out.println("A new user information entered successfully");
+        }
+        con.close();
+        }*/
+        
+        
+        
+       /* String sql="SELECT * FROM Emp1";
+       Statement st=con.createStatement();
+        ResultSet r=st.executeQuery(sql);
+        int count=1;
+        while(r.next())
+        {
+        	
+        	String name=r.getString(2);
+        	String password1=r.getString(3);
+        	String email=r.getString(4);
+        	String result="User: %d %s %s %s";
+        	System.out.println(String.format(result,count++,name,password1,email));
+        }
+        con.close();*/
        /* String sql="INSERT INTO Emp1(user_name,password,email)values(?,?,?)";
          PreparedStatement s=con.prepareStatement(sql);
        
@@ -43,7 +98,7 @@ public class CrudDb {
         	System.out.println("A new user information entered successfully");
         }
         con.close();*/
-        }
+        
            catch(SQLException e)
            {
         	   System.out.println("Exception occured" +e);
