a60df3c2780e82340702252cf09b525789a62264
srujana <srujanar308@gmail.com>
2022-05-13 00:19:22 +0530

    first commit

---
+++ Day_15_Strings/src/pack_string/Example4.java
@@ -0,0 +1,16 @@
+package pack_string;
+
+public class Example4 {
+	public static void main(String[] args) {
+		String str="Bhoomika";
+		String str1="Shetty";
+		System.out.println(str.charAt(4));
+		System.out.println(str.toLowerCase());
+		System.out.println(str.toUpperCase());
+		System.out.println(str+" "+str1);
+		System.out.println(str+" ".concat(str1));
+		String s = 50+20+50 + "error" +"raj"+ 50 + 50 + 20+"Bhoomika";  
+		System.out.println(s);
+		
+	}
+}
---
+++ Day_15_Strings/src/pack_string/Example5.java
@@ -0,0 +1,22 @@
+package pack_string;
+
+import java.util.Scanner;
+
+public class Example5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Dhoni";
+		Scanner s=new Scanner(System.in);
+		String str1=s.nextLine();
+		if(str.equals(str1))
+		{
+			System.out.println("Matching");
+		}
+		else
+		{
+			System.out.println("Not Matching");
+		}
+	}
+
+}
